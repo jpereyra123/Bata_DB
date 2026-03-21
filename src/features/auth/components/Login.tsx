aea75f30a2fe8c66efba9b58a9b9570d88c136e6
Franco <fuschendel@gmail.com>
2026-03-21 13:28:34 -0300

    Faltaba el login

--- src/features/auth/components/Login.tsx
+++ src/features/auth/components/Login.tsx
@@ -1,6 +1,6 @@
 import FormLogin from "./FormLogin";
 
-export default function LoginPage() {
+export default function Login() {
 
     return (
         <div style={{ minHeight: "100vh", display: "flex", alignItems: "center", justifyContent: "center", background: "#0f0f11" }}>
